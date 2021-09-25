86a40e074adc1216f883bcbbacad6a67f4b865db
ChengNaja <cheng_worawit@hotmail.com>
2021-09-25 22:46:17 +0700

    just another commit

---
+++ animation/circle.py
@@ -0,0 +1,12 @@
+import turtle as t
+
+t.speed(36)
+t.hideturtle()
+for i in range (120):
+  t.color('green')
+  t.circle(100)
+  t.color('black')
+  t.circle(50)
+  t.right(3)
+  t.forward(6)
+t.done()
---
+++ animation/square-circle.py
@@ -0,0 +1,10 @@
+from turtle import *
+color('red')
+speed(11)
+pensize(3)
+hideturtle()
+for i in range(60):
+  left(6)
+  for j in range(4):
+    forward(200)
+    left(90)
